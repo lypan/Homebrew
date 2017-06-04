1d2bd121db1bda51b5d2ba27ddb8a031e5032fa5
LiangYuPan <XXZERO12@gmail.com>
2017-06-05 01:54:10 +0800

    Reboot to new desktop

---
+++ install.sh
@@ -0,0 +1,9 @@
+brew install tldr
+brew install vim
+brew install git
+brew cask install alfred
+brew cask install atom
+brew cask install iterm2
+brew cask install cinch
+brew cask install github-desktop
+
