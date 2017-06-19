dea5a2daef87d702b0cdb5330ea6829320140861
LiangYuPan <XXZERO12@gmail.com>
2017-06-20 00:09:10 +0800

    Add some softwares

--- install.sh
+++ install.sh
@@ -1,14 +1,16 @@
-brew install tldr
-brew install vim
-brew install git
 brew cask install alfred
+brew cask install appcleaner
 brew cask install atom
-brew cask install iterm2
+brew cask install ccleaner
 brew cask install cinch
+brew cask install dash
+brew cask install eclipse-ide
 brew cask install github-desktop
 brew cask install google-chrome
-brew cask install eclipse-ide
+brew cask install iterm2
 brew cask install java
-brew cask install dash
 brew cask install simple-comic
 brew cask install the-unarchiver
+brew install git
+brew install tldr
+brew install vim
