817b6ca0d323eceeefa25919ebe0d197dcdfa51b
LiangYuPan <XXZERO12@gmail.com>
2017-07-02 19:41:24 +0800

    Add softwares

--- install.sh
+++ install.sh
@@ -1,21 +1,24 @@
+brew cask install adobe-acrobat-reader
 brew cask install alfred
 brew cask install appcleaner
 brew cask install atom
 brew cask install ccleaner
 brew cask install cinch
+brew cask install cmake
 brew cask install dash
 brew cask install eclipse-ide
 brew cask install github-desktop
 brew cask install google-chrome
 brew cask install iterm2
 brew cask install java
+brew cask install macvim
 brew cask install qbittorrent
 brew cask install simple-comic
+brew cask install slack
 brew cask install the-unarchiver
 brew install fzf
 brew install git
 brew install python3
+brew install the_silver_searcher
 brew install tldr
 brew install vim
-brew cask install macvim
-brew cask install cmake
