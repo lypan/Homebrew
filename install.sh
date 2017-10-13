5e09b092c0f9c0fec46656bcee43eee59b998bbd
lypan <liangyup@andrew.cmu.edu>
2017-10-13 00:56:12 -0700

    add firefox

--- install.sh
+++ install.sh
@@ -3,15 +3,11 @@ brew cask install alfred
 brew cask install appcleaner
 brew cask install atom
 brew cask install ccleaner
-brew cask install cinch
 brew cask install cmake
 brew cask install dash
-brew cask install eclipse-ide
-brew cask install github-desktop
 brew cask install google-chrome
+brew cask install firefox
 brew cask install iterm2
-brew cask install java
-brew cask install macvim
 brew cask install qbittorrent
 brew cask install simple-comic
 brew cask install slack
@@ -20,9 +16,7 @@ brew cask install vlc
 brew install emacs
 brew install fzf
 brew install git
-brew install python3
 brew install reattach-to-user-namespace
-brew install smlnj
 brew install the_silver_searcher
 brew install tldr
 brew install tmux
